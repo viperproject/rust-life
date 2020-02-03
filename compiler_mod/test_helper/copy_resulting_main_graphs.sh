ec7eb42293a762b38e303eea43f04e606349c212
Federico Poli <federpoli@gmail.com>
2020-02-03 11:44:22 +0200

    Add licence and readme

--- compiler_mod/test_helper/copy_resulting_main_graphs.sh
+++ compiler_mod/test_helper/copy_resulting_main_graphs.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+# © 2020, ETH Zurich
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 for in_example in ../collected_code/*.rs;
 do
     printf ${in_example}\\n
